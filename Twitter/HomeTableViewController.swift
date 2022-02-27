b1e5729fd928d7ecaa24b2a7ae37f3d6297dada6
CheshireCat16 <john.cheshire@gmail.com>
2022-02-27 14:28:11 -0330

    Added tweets, retweets, and likes

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -23,8 +23,19 @@ class HomeTableViewController: UITableViewController {
         
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         
+        tableView.rowHeight = UITableView.automaticDimension
+
+        tableView.estimatedRowHeight = 150
+
         tableView.refreshControl = myRefreshControl
 
+    }
+    
+
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.loadTweets()
     }
 
     @objc func loadTweets(){
@@ -34,7 +45,7 @@ class HomeTableViewController: UITableViewController {
         let tweetURL = "https://api.twitter.com/1.1/statuses/home_timeline.json"
         let params = ["count": numberOfTweets]
         
-        TwitterAPICaller.client?.getDictionariesRequest(url: tweetURL, parameters: params, success: { (tweets: [NSDictionary]) in
+        TwitterAPICaller.client?.getDictionariesRequest(url: tweetURL, parameters: params as [String : Any], success: { (tweets: [NSDictionary]) in
             
             self.tweetArray.removeAll()
             
@@ -48,7 +59,7 @@ class HomeTableViewController: UITableViewController {
             self.myRefreshControl.endRefreshing()
             
         }, failure: { (Error) in
-            print("Tweet retrieval failed")
+            print("Tweet retrieval failed \(Error)")
         })
         
         
@@ -95,6 +106,12 @@ class HomeTableViewController: UITableViewController {
         if let imageData = data {
             cell.profileImageView.image = UIImage(data: imageData)
         }
+        
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
         
         
         return cell
